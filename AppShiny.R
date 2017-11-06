2d710e6d6248a6aa48cddad81cb29f8646dfd735
Adrianna Napiórkowska <napiorkowska.adrianna@gmail.com>
2017-11-06 14:52:13 +0000

    Update AppShiny.R

--- AppShiny.R
+++ AppShiny.R
@@ -3,9 +3,9 @@ library(shiny)
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-  textInput(inputId = "title", label = "Tytu³ wykresu:", value = "Histogram rozk³adu normalnego"),
+  textInput(inputId = "title", label = "Tytul wykresu:", value = "Histogram rozkladu normalnego"),
   sliderInput(inputId = "num",label = "Liczba obserwacji:",value = 2500, min = 500, max = 5000),
-  sliderInput(inputId = "mean",label = "Œrednia:",value = 0, min = -20, max = 20),
+  sliderInput(inputId = "mean",label = "Srednia:",value = 0, min = -20, max = 20),
   sliderInput(inputId = "sd",label = "Odchylenie standardowe:",value = 25, min = 1, max = 50),
   selectInput(inputId = "colour", label = "Wybierz kolor:", choices = c("blue","red","green","yellow"))
   ),
